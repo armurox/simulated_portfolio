e655cc1a63ffa231a3d0baaeb1e086b5ad9b85e9
Arman Jasuja <arman_jasuja@yahoo.com>
2023-08-12 12:33:08 +0530

    Show password feature .js file added

---
+++ static/show_password.js
@@ -0,0 +1,16 @@
+document.addEventListener('DOMContentLoaded', function(){
+    let passwords = document.querySelectorAll('.plac');
+    let checkbox = document.querySelector('#checkbox');
+    console.log(passwords.length);
+    checkbox.addEventListener('click', function(){
+        for (password of passwords) {
+            if (password.type == "password"){
+                password.type = "text";
+            }
+
+            else{
+                password.type = "password";
+            }
+        }
+    });
+});
